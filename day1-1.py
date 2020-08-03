889e05ee30008ee20506c6842595068b816e4be3
monkey0525 <69131612+monkey0525@users.noreply.github.com>
2020-08-03 11:16:24 +0800

    Add files via upload

---
+++ day1-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:06:12 2020
+
+@author: user
+"""
+print("I'm Eric \n He,is 'cool'")
+
+
---
+++ day1-2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:27:36 2020
+
+@author: user
+"""
+name=input("報上名來:")
+print("hi"+name)
