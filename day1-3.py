51afa1ec94288acd95c5a66fa5c9b5f28a2a1de9
monkey0525 <69131612+monkey0525@users.noreply.github.com>
2020-08-03 11:54:57 +0800

    Add files via upload

---
+++ day1-3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 11:24:04 2020
+
+@author: user
+"""
+pi=input("半徑:")
+print("面積",int(pi)*int(pi)*3.14159265358979383642)
+print("圓周長",2*int(pi)*int(pi))
