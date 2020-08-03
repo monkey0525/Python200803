4cacb96c90115f5fde926ae9eaaa38283f34e4fb
monkey0525 <69131612+monkey0525@users.noreply.github.com>
2020-08-03 13:46:35 +0800

    Delete day1-3.py

--- day1-3.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug  3 11:24:04 2020
-
-@author: user
-"""
-pi=input("半徑:")
-print("面積",int(pi)*int(pi)*3.14159265358979383642)
-print("圓周長",2*int(pi)*int(pi))
